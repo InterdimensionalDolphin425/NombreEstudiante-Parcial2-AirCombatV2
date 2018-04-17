717a669b89f7f1778ff9a72f436347315eaa3d40
Pablo González <interdimensionaldolphin@gmail.com>
2018-04-16 19:20:13 -0500

    Entrega Parcial 2 con Tercer OVNI

---
+++ PabloGonzalez-AirCombatV2/Assets/Scripts/Airplane.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            this.transform.Translate(0.0f, 0.6f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+            this.transform.Translate(0.0f, -0.6f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            this.transform.Translate(-0.8f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            this.transform.Translate(0.8f, 0.0f, 0.0f);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            GameObject missile = GameObject.Instantiate(Resources.Load("Prefabs/Missile") as GameObject);
+
+
+            missile.transform.SetParent(this.gameObject.transform);
+
+            missile.transform.localPosition = new Vector3(0.0f, -2.101f, 0.783f);
+
+            missile.transform.SetParent(null);
+
+
+        }
+
+        this.transform.Translate(0.0f, 0.0f, 28 * Time.deltaTime);
+
+    }
+}
